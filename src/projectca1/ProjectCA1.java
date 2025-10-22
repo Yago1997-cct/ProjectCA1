b7ffc0f3b82ba3b319c63d055049611157269dd4
Yago1997-cct <yagoguisilva1997@gmail.com>
2025-10-22 11:45:15 +0100

    1 commit - Try to cread a Food app where I stated my code by identifying the project's keywords and putting specifications as a int and string

--- src/projectca1/ProjectCA1.java
+++ src/projectca1/ProjectCA1.java
@@ -4,17 +4,40 @@
  */
 package projectca1;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 /**
  *
  * @author yagog
  */
 public class ProjectCA1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Test code");
-    }
     
+ public class Food {
+    public String name;   
+    private int weight;
+    private int bbd;          
+    private int timeplace;    
+
+    public Food(String name, int weight, int bbd, int timeplace) {
+        this.name = name;
+        this.weight = weight;
+        this.bbd = bbd;
+        this.timeplace = timeplace;
+    }
+
+    
+    public static Optional<Food> safeCreate(String name, int weight, int bbd, int timeplace) {
+        if (name == null || name.isBlank()) {
+            return Optional.empty();
+        }
+        if (weight <= 0) {
+            return Optional.empty();
+        }
+        if (bbd < 1 || bbd > 14) {
+            return Optional.empty();
+        }
+        return Optional.of(new Food(name.trim(), weight, bbd, timeplace));
+    }
+}
 }
